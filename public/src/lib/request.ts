9287160384e8f2a4c91ea8a6ac1a7cdb047aa3be
Miguel[1230199] <1230199@isep.ipp.pt>
2024-11-08 16:37:01 +0000

    feat: create a configurated request function to the api

---
+++ public/src/lib/request.ts
@@ -0,0 +1,47 @@
+import { apiConfig, apiKey } from "./config";
+import { Decrypt, Encrypt } from "./encryption";
+
+const { baseUrl } = apiConfig;
+
+export async function SendRequest(
+  path: string,
+  method: string,
+  data: Record<string, string> = {},
+  fieldsToEncrypt: string[] = [],
+  token = ""
+): Promise<Response> {
+  const headers: Record<string, string> = {
+    "Content-type": "application/json",
+    Authorization: "",
+  };
+  headers["X-API-KEY"] = apiKey;
+  if (token !== "") {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  const processedData: Record<string, string> = { ...data };
+  if (["POST", "PATCH", "PUT"].includes(method.toUpperCase()) && data) {
+    for (const key of fieldsToEncrypt) {
+      if (processedData[key]) {
+        const encryptedValue = Encrypt(processedData[key]);
+        if (encryptedValue) {
+          processedData[key] = await encryptedValue;
+        } else {
+          console.warn(`Encryption failed for field: ${key}`);
+        }
+      }
+    }
+  }
+
+  const options: RequestInit = {
+    method: method,
+    headers: headers,
+    mode: "cors",
+    credentials: "include",
+    body: JSON.stringify(processedData),
+  };
+
+  const response = await fetch(`${baseUrl}${path}`, options);
+
+  return response;
+}
