62db4de3657535fddf76035f71c69d69d1eb0def
Miguel[1230199] <1230199@isep.ipp.pt>
2024-11-23 14:58:38 +0000

    fix: remove unsused base api url

--- public/src/lib/eventRequests.ts
+++ public/src/lib/eventRequests.ts
@@ -1,71 +1,68 @@
 import axios from "axios";
-import { apiConfig } from "./config";
+import { apiConfig, apiKey } from "./config";
 
 const { baseUrl } = apiConfig;
-const API_BASE_URL = `${baseUrl}/api/events`;
-
-// Event model interface
 
 export async function fetchAllEvents(token: string = ""): Promise<any[]> {
-    try {
-      const response = await fetch(`${baseUrl}/api/events`, {
-        method: "GET",  
-        headers: {
-          "Content-Type": "application/json", 
-          "X-API-KEY": apiKey,  
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error("Error fetching events");
-      }
+  try {
+    const response = await fetch(`${baseUrl}/api/events`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": apiKey,
+      },
+    });
 
-      return await response.json();
-    } catch (error) {
-      console.error("Error fetching events:", error);
-      return [];
-    }
-}
-    export async function fetchNonPromotedEvents(token: string = ""): Promise<any[]> {
-      try {
-        const response = await fetch(`${baseUrl}/api/events/non-promoted`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "X-API-KEY": apiKey,
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error("Error fetching events");
-        }
-
-        return await response.json();
-      } catch (error) {
-        console.error("Error fetching events:", error);
-        return [];
-      }
+    if (!response.ok) {
+      throw new Error("Error fetching events");
     }
 
-    export async function fetchPromotedEvents(token: string = ""): Promise<any[]> {
-      try {
-        const response = await fetch(`${baseUrl}/api/events/promoted`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "X-API-KEY": apiKey,
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error("Error fetching events");
-        }
-
-        return await response.json();
-      } catch (error) {
-        console.error("Error fetching events:", error);
-        return [];
-      }
-
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    return [];
+  }
 }
-  
+export async function fetchNonPromotedEvents(
+  token: string = ""
+): Promise<any[]> {
+  try {
+    const response = await fetch(`${baseUrl}/api/events/non-promoted`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": apiKey,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error fetching events");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    return [];
+  }
+}
+
+export async function fetchPromotedEvents(token: string = ""): Promise<any[]> {
+  try {
+    const response = await fetch(`${baseUrl}/api/events/promoted`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": apiKey,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error fetching events");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    return [];
+  }
+}
