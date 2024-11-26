235eda95342c0050dc2f53d32740dcb687b26f11
Miguel[1230199] <1230199@isep.ipp.pt>
2024-11-26 19:01:17 +0000

    feat: print local ip of api to use it for mobile debugging

--- src/main/java/isep/ipp/pt/Smart_cities/SmartCitiesApplication.java
+++ src/main/java/isep/ipp/pt/Smart_cities/SmartCitiesApplication.java
@@ -1,13 +1,28 @@
 package isep.ipp.pt.Smart_cities;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.net.InetAddress;
 
 @SpringBootApplication
 public class SmartCitiesApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(SmartCitiesApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(SmartCitiesApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SmartCitiesApplication.class, args);
+
+		try {
+
+			String localIp = InetAddress.getLocalHost().getHostAddress();
+			int port = context.getEnvironment().getProperty("server.port", Integer.class, 8080);
+
+			logger.info("Application running at http://{}:{}", localIp, port);
+		} catch (Exception e) {
+			logger.error("Error getting local IP address: " + e.getMessage());
+		}
 	}
 
 }
