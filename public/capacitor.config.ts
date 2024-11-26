2a01eefb4a96fdc349b3f16853132e91320b48cd
Miguel[1230199] <1230199@isep.ipp.pt>
2024-11-26 18:59:50 +0000

    chore: configure android to allow http request; connect to FCM(firebase cloud message); ask for notification permission

--- public/capacitor.config.ts
+++ public/capacitor.config.ts
@@ -1,9 +1,23 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'public',
-  webDir: 'dist'
+  appId: "io.ionic.starter",
+  appName: "public",
+  webDir: "dist",
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+    },
+    PushNotifications: {
+      presentationOptions: ["badge", "alert", "sound"],
+    },
+  },
+  android: {
+    allowMixedContent: true,
+  },
+  server: {
+    allowNavigation: ["*"],
+  },
 };
 
 export default config;
