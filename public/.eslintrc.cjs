34cc06dd83e9727bfb2b3fd3658c7d33e29e8c83
Miguel[1230199] <1230199@isep.ipp.pt>
2024-11-27 23:17:01 +0000

    feat: add formatter and pre-commit hooks

--- public/.eslintrc.cjs
+++ public/.eslintrc.cjs
@@ -3,10 +3,12 @@ module.exports = {
   env: {
     node: true
   },
+  plugins: ["@typescript-eslint"],
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2020
