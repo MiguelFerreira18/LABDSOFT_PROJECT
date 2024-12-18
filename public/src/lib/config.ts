7fe08072d6277de3df02e6d2053d700bdc7bb37f
Miguel[1230199] <1230199@isep.ipp.pt>
2024-12-18 16:50:10 +0000

    feat: configure production environment for mobile app

--- public/src/lib/config.ts
+++ public/src/lib/config.ts
@@ -6,12 +6,14 @@ const devApiConfig: ServerStruct = {
 };
 
 const prodApiConfig: ServerStruct = {
-  baseUrl: 'http://localhost:9091',
+  baseUrl: 'https://tarpon-allowed-moray.ngrok-free.app',
 };
 
 interface ServerStruct {
   baseUrl: string;
 }
+
+console.log('isProduction', isProduction);
 
 const apiConfig = isProduction ? prodApiConfig : devApiConfig;
 export { apiConfig, apiKey };
