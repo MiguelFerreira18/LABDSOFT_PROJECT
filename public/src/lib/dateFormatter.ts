071e0fcdff8bd97455ae9b8ca82e2321af5391e8
DanielNeves13 <1230175@isep.ipp.pt>
2024-12-19 18:56:31 +0000

    fix: dateFormatter formated

--- public/src/lib/dateFormatter.ts
+++ public/src/lib/dateFormatter.ts
@@ -1,8 +1,18 @@
 // Função para mapear o número do mês para a abreviação do mês
 function getMonthName(monthNumber: number): string {
   const months = [
-    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
   ];
 
   return months[monthNumber - 1] || ''; // Retorna o mês correspondente, ou uma string vazia se inválido
